8b4ad12c6f01c0945cff6791bce5a353f8311d60
Gustavo <gustavocaraujo@gmail.com>
2017-06-03 14:19:10 -0300

    teste sleep milliseconds

---
+++ first-html/index.js
@@ -0,0 +1,11 @@
+console.log("Hi there");
+
+function sleep (milliseconds) {
+    let startTime = Date.now();
+    while(Date.now()< startTime + milliseconds);
+}
+
+sleep(2000);
+
+console.log("Hi there after sleep");
+
